a70ecff18bec71d06a6801fe62d2bd8deb6eb8b5
Kamil Marynowski <kamil.marynovski@gmail.com>
2023-10-20 19:17:23 +0200

    dev: Add register controller & view

---
+++ cs_todo/Controllers/Auth/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cs_todo.Controllers.Auth
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
